85c09ab8aeb459e10567191a7d909414e0350e63
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-04 09:36:31 +0700

    rename main application

--- src/main/java/com/springbootredis/SpringBootRedisApplication.java
+++ src/main/java/com/springbootredis/SpringBootRedisApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.springbootredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringBootRedisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringBootRedisApplication.class, args);
 	}
 
 }
--- src/test/java/com/springbootredis/SpringBootRedisApplicationTests.java
+++ src/test/java/com/springbootredis/SpringBootRedisApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.springbootredis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootRedisApplicationTests {
 
 	@Test
 	void contextLoads() {
