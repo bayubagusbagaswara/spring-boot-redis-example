cd27b4bac06fd0771e7125a52dec781e3a427da0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-02 13:07:07 +0700

    update IEmployeeDao

--- src/main/java/com/springbootredis/dao/IEmployeeDao.java
+++ src/main/java/com/springbootredis/dao/IEmployeeDao.java
@@ -1,2 +1,16 @@
-package com.springbootredis.dao;public interface IEmployeeDao {
+package com.springbootredis.dao;
+
+import com.springbootredis.model.Employee;
+
+import java.util.Map;
+
+public interface IEmployeeDao {
+
+    void saveEmployee(Employee employee);
+    Employee getOneEmployee(Integer id);
+    void updateEmployee(Employee employee);
+    Map<Integer, Employee> getAllEmployees();
+    void deleteEmployee(Integer id);
+    void saveAllEmployees(Map<Integer, Employee> map);
+
 }
