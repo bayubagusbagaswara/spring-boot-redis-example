757d13915a910cf426f9a942176bcbcbe2e5d9d5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-02 13:06:44 +0700

    update EmployeeDaoImpl

--- src/main/java/com/springbootredis/dao/EmployeeDaoImpl.java
+++ src/main/java/com/springbootredis/dao/EmployeeDaoImpl.java
@@ -1,2 +1,41 @@
-package com.springbootredis.dao;public class EmployeeDaoImpl {
+package com.springbootredis.dao;
+
+import com.springbootredis.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public class EmployeeDaoImpl implements IEmployeeDao {
+
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public Employee getOneEmployee(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void updateEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public Map<Integer, Employee> getAllEmployees() {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Integer id) {
+
+    }
+
+    @Override
+    public void saveAllEmployees(Map<Integer, Employee> map) {
+
+    }
 }
