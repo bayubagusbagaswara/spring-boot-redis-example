f53c15cc4583ac65bd3b5de1c29c6e81cba75f92
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-02 13:07:19 +0700

    update model Employee

--- src/main/java/com/springbootredis/model/Employee.java
+++ src/main/java/com/springbootredis/model/Employee.java
@@ -1,2 +1,22 @@
-package com.springbootredis.model;public class Employee {
+package com.springbootredis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+// No @Entity concept here
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = -6536313623986629700L;
+
+    private Integer employeeId;
+    private String employeeName;
+    private Double employeeSalary;
 }
