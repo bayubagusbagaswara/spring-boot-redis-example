6c4efd09725f8a6912209d1c9a7fefb24a912320
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-02 13:06:12 +0700

    update AppConfig

--- src/main/java/com/springbootredis/config/AppConfig.java
+++ src/main/java/com/springbootredis/config/AppConfig.java
@@ -1,2 +1,31 @@
-package com.springbootredis.config;public class AppConfig {
+package com.springbootredis.config;
+
+import com.springbootredis.model.Employee;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Connection between application & Redis DB
+ * RedisConnectionFactory (Interface) mempunyai 2 implementasi, yakni JedisConnectionFactory and LettuceConnectionFactory
+ * Lettuce is the latest driver.
+ */
+@Configuration
+public class AppConfig {
+
+    // Creating Connection with Redis
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    // Creating RedisTemplate for Entity 'Employee'
+    @Bean
+    public RedisTemplate<String, Employee> redisTemplate() {
+        RedisTemplate<String, Employee> empTemplate = new RedisTemplate<>();
+        empTemplate.setConnectionFactory(redisConnectionFactory());
+        return empTemplate;
+    }
 }
